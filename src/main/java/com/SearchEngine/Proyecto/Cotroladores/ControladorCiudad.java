964c1393a0718516c9cf346caf9c068254aabf0a
SantiTech-HM <shoyo01@misena.edu.co>
2024-09-29 15:51:23 -0500

    CRUD entidad ciudad

--- src/main/java/com/SearchEngine/Proyecto/Cotroladores/ControladorCiudad.java
+++ src/main/java/com/SearchEngine/Proyecto/Cotroladores/ControladorCiudad.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("api/v4/")
 @RestController
@@ -22,7 +24,7 @@ public class ControladorCiudad {
         return isCiudad.listarCiudad();
     }
     @GetMapping("ciudad/{id}")
-    public ResponseEntity<Ciudad> buscarCiudad(@PathVariable Long id) {
+    public ResponseEntity<Ciudad> buscarCiudadPorId(@PathVariable Long id) {
         Ciudad ciudad = isCiudad.buscarCiudadPorId(id);
         if (ciudad == null){
             throw new ExcepcionGlobal("ID de la ciudad no encontrado" + id);
@@ -31,10 +33,34 @@ public class ControladorCiudad {
     }
 
     @PostMapping("ciudad")
-    public Ciudad guardarCiudad (@RequestBody Ciudad ciudad){
+    public Ciudad guardarCiudad(@RequestBody Ciudad ciudad){
         return isCiudad.guardarCliudad(ciudad);
     }
 
     @PutMapping("ciudad/{id}")
-    
+    public ResponseEntity<Ciudad> buscarCiudadPorId(@PathVariable Long id, @RequestBody Ciudad ciudadObjeto) {
+        Ciudad ciudad = isCiudad.buscarCiudadPorId(id);
+        if (ciudad == null){
+            throw new ExcepcionGlobal("Ciudad no encontrada");
+        }
+        ciudad.setCiudad(ciudadObjeto.getCiudad());
+        ciudad.setId(ciudadObjeto.getId());
+        ciudad.setNombre(ciudad.getNombre());
+
+        Ciudad actualizarCiudad = isCiudad.guardarCliudad(ciudad);
+        return ResponseEntity.ok(actualizarCiudad);
+    }
+    @DeleteMapping("ciudad/{id}")
+    public ResponseEntity<Map<String,Boolean>> eliminarCiudad(@PathVariable Long id) {
+        Ciudad ciudad = isCiudad.buscarCiudadPorId(id);
+        if (ciudad == null) {
+            throw new ExcepcionGlobal("id de continente no encontrado" + id);
+
+        }
+        isCiudad.eliminnarCiudad(ciudad);
+        Map<String,Boolean> respuesta = new HashMap<>();
+        respuesta.put("Ciudad eliminada", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
+    }
+
 }
